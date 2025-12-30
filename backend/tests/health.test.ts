53e99d438f1db4649d0e7eb89dfff2bcd39cb7a5
Arpit Chauhan <arpit@Arpits-MacBook-Air.local>
2025-12-30 15:10:50 +0530

    cd-staging test 1

--- backend/tests/health.test.ts
+++ backend/tests/health.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 import request from 'supertest'
 import app from '../src/app.js'
 
-describe("Test /health endpoint", () => {
+describe("Test for /health endpoint", () => {
     it("should return 200 and OK", async () => {
         const res = await request(app).get("/health")
         expect(res.status).toBe(200)
